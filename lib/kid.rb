ced2badc74a716aab8f30a20f3fabcb79406b339
Xavier Perez <xperez007@gmail.com>
2020-02-24 12:41:54 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,22 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+
+class Kid 
+  include Dance 
+  extend MetaDancing
+  extend 
+FancyDance::ClassMethods 
+  include
+FancyDance::InstanceMethods
+end
+
+end
+
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+
+end
